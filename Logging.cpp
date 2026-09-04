2c9b148974b3e80a1df743334c1fc1488e0377f8
Victor Agu <victoragu14x@gmail.com>
2026-09-04 16:21:52 +0100

    append_file function works independently wthout file_create

--- Logging.cpp
+++ Logging.cpp
@@ -1,24 +1,31 @@
+/*------------------- WRITTEN BY VICTOR AGU ------------------ */
+/* 
+MY FIRST C++ Library 
+NO AI WAS USED IN THE PRODUCTION OF THIS CODE 
+
+*/
 #include<iostream>
+#include<format>
 #include<filesystem>
 #include<fstream>
-#include<ctime>
-
+#include<chrono>
 using namespace std ;
 namespace os  = std :: filesystem ;
 
 
 // ==== Configs and Hyper Params ==== //
 
-time_t current_time = time(&current_time) ;
+
 os :: path cwd = os :: current_path() ;
-char *time_now = ctime(&current_time) ;
+
 
 fstream f;
 
-enum class LEVEL{
+enum class LEVEL{ // i shall add log level no when the need arises , dont really see the use to add them 
     INFO ,
     DEBUG ,
-    WARNING , 
+    WARNING ,
+    ERROR , 
     CRITICAL ,
 
 }  ;
@@ -27,13 +34,12 @@ enum class LEVEL{
 bool file_create(fstream &File ) {
     try {
     // create file empty file 
-    if (true) { 
-        
-        File << " ================= LOG FILE ===================== " ;
-        cout<< "INFO : -> [ FILE CREATED ] " ;
-        return true ; 
-    }    
-    else {
+    if (File){
+        File << " ================= LOG FILE ===================== "<<'\n' ;
+        cout<< "INFO : -> [ FILE CREATED ] " ; return true ;
+    }
+    
+    if(!File) {
         throw runtime_error(" [ Opening File ]")  ;
         return false ;        
         }
@@ -49,6 +55,7 @@ void append_file(os :: path &File , string &message , LEVEL &level ) { // openfi
 
     fstream file (File , ios ::app) ; 
     string warning_level ; 
+    auto time = chrono ::system_clock::now() ;
 
     if(level == LEVEL :: INFO ) {
         warning_level = "INFO " ;
@@ -60,6 +67,9 @@ void append_file(os :: path &File , string &message , LEVEL &level ) { // openfi
         warning_level =  "WARNING " ;
 
     }
+    else if (level == LEVEL ::ERROR) {
+        warning_level = "ERROR : " ;
+    }
     else if (level == LEVEL ::CRITICAL) {
         warning_level = "CRITICAL : " ;
     }
@@ -69,22 +79,24 @@ void append_file(os :: path &File , string &message , LEVEL &level ) { // openfi
         throw std :: runtime_error (" FILE NOT FOUND ") ;
     }
     else {
-        file << "TIME : " << time_now <<warning_level<<message << " ] "<< "\n" ;
+        string message_formated = std :: format (" TIME : {} | {} [  {}  ]",time ,warning_level ,message);
+        file <<message_formated ;
         }
     }catch(std :: runtime_error &e){
         cerr << "ERROR " << e.what() << '\n'; 
     }
     
 }
-
-
+void LOG_CONSOLE (string &message , LEVEL &level) { // Function to log to console and not file  
+    
+}
 namespace LOG {
 
     class logging {
-        public :
+        public : //basically i dont really see the ude of adding private for the filenames cause it just a logging library right ? 
             string filename ; 
             bool log_console ;
-        
+
             void log_file(string &message , LEVEL level ){ // for file saving only first 
                
                 os :: path log_dir = "LOG" ;
@@ -93,41 +105,58 @@ namespace LOG {
                 if (! os :: exists(log_folder)) {
                     os :: create_directory(log_folder) ;
                 }
-
                 fstream File(log_dir/filename) ;
                 file_create(File) ; // create file
-                
                 os :: path file_append = log_folder /filename ; // path to log file 
 
+            
                 if (level == LEVEL ::INFO ){
-                    
-                    append_file(file_append ,message, level ) ;
-                    
-                    if (log_console == true ) {
-                        File<<"TIME : " << time_now <<" INFO : [ " <<message << " ] "<< "\n" ;                
-                        cerr <<" INFO : " <<message << '\n' ; 
-                    }
-                    else {
-                        File<<"TIME : " << time_now <<" INFO : [ " <<message << " ] "<< "\n" ;                
+                append_file(file_append ,message, level ) ;     
+                if (log_console == true ) {
+                    cerr <<" INFO : " <<message << '\n' ; 
                     }
                 }
-                // if (level == LEVEL ::DEBUG ) { 
-                //     File<<"TIME : " << time_now <<" INFO : [ " <<message << " ] "<< "\n" ;
-                    
-                // }
 
-
+                if (level == LEVEL ::DEBUG ) { 
+                    append_file(file_append ,message, level ) ;
+                                       
+                    if (log_console == true ){
+                        cerr <<" DEBUG : " <<message << '\n' ; 
+                    }
+                }
+                if (level == LEVEL ::ERROR){
+                    append_file(file_append ,message, level ) ;
+                    if (log_console == true){
+                        cerr <<" ERROR : " <<message << '\n' ;                        
+                    }
+                }
+                if (level == LEVEL ::WARNING ){
+                    append_file(file_append ,message, level ) ;
+                    if (log_console == true){
+                        cerr <<" WARNING : " <<message << '\n' ;                        
+                    }
+                }
+                if (level == LEVEL ::CRITICAL){
+                    append_file(file_append ,message, level ) ;
+                    if(log_console ==true ){
+                        cerr <<" CRITICAL : " <<message << '\n' ; 
+                    }
+                }
+                
             }
     }; 
 
 }
 
+
+// =================== Testing Area ======================= //
 int main () { 
 
-    LOG :: logging log("text.txt" ,false) ;
+    LOG :: logging log("text.txt" ,true) ;
     string mesage = "Hellfdfdfo" ;
+    string mes_age = "Hsasasa" ;
 
     log.log_file(mesage,LEVEL :: INFO) ;
-
+    log.log_file(mes_age , LEVEL::DEBUG) ;
 
 }
